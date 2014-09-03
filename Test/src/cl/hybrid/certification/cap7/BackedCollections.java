03a0da100078278d0802de04ff5e891ed5a35d4d
CMCC0747 <CMCC0747@W7-MCONTRERAS.transbank.local>
2014-09-03 15:14:24 -0400

    Colecciones Respaldadas.

---
+++ Test/src/cl/hybrid/certification/cap7/BackedCollections.java
@@ -0,0 +1,38 @@
+package cl.hybrid.certification.cap7;
+
+import java.util.TreeMap;
+import java.util.SortedMap;
+import java.util.Map;
+
+
+public class BackedCollections {
+
+	
+	public static void main(String[] args){
+		
+		TreeMap<Integer,String> treeMap = new TreeMap<Integer,String>();
+		SortedMap<Integer,String> subTreeMap = null;
+		
+		treeMap.put(1,"uno");
+		treeMap.put(2,"dos");
+		treeMap.put(3,"tres");
+		treeMap.put(4,"cuatro");
+		treeMap.put(5,"cinco");
+		treeMap.put(6,"seis");
+		
+		subTreeMap = treeMap.subMap(3, 10);
+		imprime(subTreeMap);
+		subTreeMap.put(7, "siete");
+		imprime(subTreeMap,treeMap);
+		treeMap.remove(4);
+		imprime(subTreeMap,treeMap);
+		
+	}
+	
+	private static void imprime(Map<Integer, String>... maps){
+		for(Map<Integer,String> map : maps){
+			System.out.println(map);
+		}
+		
+	}
+}
